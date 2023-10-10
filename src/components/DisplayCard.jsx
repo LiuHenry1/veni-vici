2f1683baa8c4b985e82dc92dc621947811ff16bf
Henry Liu <hliu4176@gmail.com>
2023-10-10 16:57:00 -0500

    Feature: Implement functionality for card component
    Card now displays a list of attributes.

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -1,8 +1,26 @@
-const DisplayCard = ({ dog }) => {
+const DisplayCard = ({ dog, handleClick }) => {
+  // Representation of each attribute for display
+  const displayRepr = {
+    breed_group: "Breed Group",
+    weight: "Weight",
+    height: "Height",
+    life_span: "Life Span",
+  };
+
   return (
     <>
       <h3>{dog.name}</h3>
       <div className="attribute-container">
+        {Object.entries(dog.attributes).map(([key, value]) => {
+          if (value != undefined) {
+            return (
+              <div className="attribute">
+                <h4>{displayRepr[key]}</h4>
+                <button onClick={handleClick}>{value}</button>
+              </div>
+            );
+          }
+        })}
       </div>
       <img src={dog.url} width="600px" height="500px" />
     </>
