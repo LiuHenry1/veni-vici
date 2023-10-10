fcb0cc16277144de4581a18d650abc7293fc357b
Henry Liu <hliu4176@gmail.com>
2023-10-10 14:29:46 -0500

    Feat: implement function to fetch info from API
    The function serves as a callback for the event listener attached to the
    button with the text "Discover."

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,48 @@ import axios from 'axios';
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
+  // list of desired attributes to obtain from API
+  const attributes = [
+    "breed_group",
+    "weight",
+    "height",
+    "name",
+    "life_span",
+    "temperament"
+  ]
+
+  const [currentDog, setDog] = useState(null);
+
+  const callAPI = async() => {
+    // Fetch only data that has breed data attached
+    const base_url = "https://api.thedogapi.com/v1/images/search?has_breeds=1"
+
+    // Fetch data 
+    const response = await axios.get(base_url, {
+      headers: {
+        "x-api-key": API_KEY,
+        "content-type": "application/json"
+      }
+    }).catch(error => {
+      alert(error.response.status);
+    })
+
+    // Get necessary data
+    const data = response.data[0];
+    
+    // Extract the attributes we want and return a dog object with those 
+    // properties
+    const extractAttributes = ({breeds: [info], url}) => {
+      const dog = {};
+      attributes.map(attribute => dog[attribute] = info[attribute]);
+      dog["url"] = url;
+      
+      return dog;
+    }
+
+    const newDog = extractAttributes(data);
+    setDog(newDog);
+  }
 
 
   return (
@@ -12,7 +54,7 @@ function App() {
     <>
     <h1>Dog Overdose</h1>
     <h4>Discover dogs of every kind!</h4>
-    <button onClick={handleClick}>Discover!</button>
+    <button onClick={callAPI}>Discover!</button>
     </>
   )
 }
